31bec58d69e0db8a41156cdb57cc46386a01cdb7
Patrick Tomasi <info@tomasi-developing.ch>
2021-11-29 09:43:11 +0100

    Day 35

--- Advanced JavaScript/app.js
+++ Advanced JavaScript/app.js
@@ -24,3 +24,50 @@ let firstParagraph = document.querySelector('p');
 // 3. Insert the new element into the parent element content
 
 firstParagraph.append(newAnchorElement);
+
+// REMOVE ELEMENTS
+// 1. Select the element that should removed
+
+let firstH1Element = document.querySelector('h1');
+
+// 2. Remove it!
+
+firstH1Element.remove();
+// firstH1Element.parentElement.removeChild(firstH1Element); // for older browsers
+
+// MOVE ELEMENTS
+
+firstParagraph.parentElement.append(firstParagraph);
+
+// innerHTML
+
+console.log(firstParagraph.textContent);
+console.log(firstParagraph.innerHTML);
+
+firstParagraph.innerHTML = 'Hi! This is <strong>important!</strong>';
+
+// EVENT - LISTENER
+
+let paragraphElement = document.body.querySelector('p');
+paragraphElement.textContent = 'Click me!';
+
+function changeParagraphText(event) {
+    paragraphElement.textContent = 'Clicked!';
+    console.log(event);
+    console.log('Paragraph clicked!');
+}
+
+paragraphElement.addEventListener('click', changeParagraphText);
+
+let inputElement = document.querySelector('input');
+
+function retrieveUserInput(event) {
+    // let entredText = inputElement.value;
+    let entredText = event.target.value; 
+    let entredData = event.data; // => This is different!
+    console.log(entredText);
+    console.log(entredData);
+    console.log(event);
+}
+
+inputElement.addEventListener('input', retrieveUserInput);
