4ac10b5815cd273384c980a04e19186c11623211
Patrick Tomasi <info@tomasi-developing.ch>
2021-11-29 08:38:24 +0100

    Day 34

--- Advanced JavaScript/app.js
+++ Advanced JavaScript/app.js
@@ -2,4 +2,25 @@ console.log(window);
 console.log(document);
 console.dir(document);
 
-document.body.children[1].children[0].href = 'https://www.google.com';
+// document.body.children[1].children[0].href = 'https://www.google.com';
+
+let anchorElement = document.getElementById('external-link');
+anchorElement.href = 'https://www.google.com';
+
+anchorElement = document.querySelector('p a');
+anchorElement.href = 'http://www.google.ch';
+
+// ADD AN ELEMENT
+// 1. Create the new element
+
+let newAnchorElement = document.createElement('a');
+newAnchorElement.href = 'https://www.google.ch';
+newAnchorElement.textContent = 'This leads to Google!';
+
+// 2. Get access to the parent element that should hold the new element
+
+let firstParagraph = document.querySelector('p');
+
+// 3. Insert the new element into the parent element content
+
+firstParagraph.append(newAnchorElement);
