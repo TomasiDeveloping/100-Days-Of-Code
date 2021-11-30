1da689ee5644454a8aa1cd9f06e0aef694297e1e
Patrick Tomasi <info@tomasi-developing.ch>
2021-11-30 13:12:19 +0100

    DAY 38

--- Advanced JavaScript/demo/conditional-code.js
+++ Advanced JavaScript/demo/conditional-code.js
@@ -3,3 +3,16 @@ const myName = 'Patrick';
 if (myName === 'Patrick') {
     console.log('Hello!');
 }
+
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log('User is logged in!');
+}
+
+// Truthy & Falsy
+const entredUserName = 'Patrick';
+
+if (entredUserName) {
+    console.log('Input is valid!');
+}
