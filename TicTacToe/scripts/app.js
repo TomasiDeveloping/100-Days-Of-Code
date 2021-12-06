276f36ae4e0c16ec94e9ab5cf2972a79ddfb0c1c
Patrick Tomasi <info@tomasi-developing.ch>
2021-12-06 11:44:23 +0100

    DAY 43

--- TicTacToe/scripts/app.js
+++ TicTacToe/scripts/app.js
@@ -1,4 +1,12 @@
+const gameData = [
+    [0, 0, 0],
+    [0, 0, 0,],
+    [0, 0, 0],
+];
+
 let editedPlayer = 0;
+let activePlayer = 0;
+let currentRound = 1;
 
 const players = [
     {
@@ -15,11 +23,15 @@ const playerConfigOverlayElement = document.getElementById('config-overlay');
 const backdropElement = document.getElementById('backdrop');
 const formElement = document.querySelector('form');
 const errorsOutputElement = document.getElementById('config-error');
+const gameAreaElement = document.getElementById('active-game');
+const avtivePlayerNameElement = document.getElementById('active-player-name');
 
 const editPlayer1BtnElement = document.getElementById('edit-player-1-btn');
 const editPlayer2BtnElement = document.getElementById('edit-player-2-btn');
 const cancelConfigBtnElement = document.getElementById('cancel-config-btn');
-
+const startNewGameBtnElement = document.getElementById('start-game-btn');
+// const gameFieldElements = document.querySelectorAll('#game-board li');
+const gameBoardElement = document.getElementById('game-board');
 
 editPlayer1BtnElement.addEventListener('click', openPlayerConfig);
 editPlayer2BtnElement.addEventListener('click', openPlayerConfig);
@@ -28,3 +40,11 @@ cancelConfigBtnElement.addEventListener('click', closePlayerConfig);
 backdropElement.addEventListener('click', closePlayerConfig);
 
 formElement.addEventListener('submit', savePlayerConfig);
+
+startNewGameBtnElement.addEventListener('click', startNewGame);
+
+// for (const gameFieldElemet of gameFieldElements) {
+//     gameFieldElemet.addEventListener('click', selectGameField);
+// }
+
+gameBoardElement.addEventListener('click', selectGameField);
