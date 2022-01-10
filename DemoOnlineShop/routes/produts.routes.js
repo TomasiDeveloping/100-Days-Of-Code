edd30bc0034d643cc8ad5ece6f507dfa0eed9e67
Patrick Tomasi <info@tomasi-developing.ch>
2022-01-10 14:38:44 +0100

    DAY 88

--- DemoOnlineShop/routes/produts.routes.js
+++ DemoOnlineShop/routes/produts.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const produtsController = require('../controllers/products.controller');
+
 const router = express.Router();
 
-router.get('/products', function(req, res) {
-    res.render('customer/products/all-products');
-});
+router.get('/products', produtsController.getAllProducts);
+
+router.get('/products/:id', produtsController.getProductDetails);
 
 module.exports = router;
